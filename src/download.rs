bc3597fce825f43866bfb6f355066818b27d2097
kame1366 <110384579+kame1366@users.noreply.github.com>
2024-11-12 23:48:58 +0900

    Updated gitignore and the obtained data was arrayed

--- src/download.rs
+++ src/download.rs
@@ -1,17 +1,35 @@
 fn get_samplecase(url_id: String) -> Result<(), Box<dyn std::error::Error>> {
     let url= format!("https://atcoder.jp/contests/{url_id}/tasks/{url_id}");
-    let problem_id = vec!["_a", "_b", "_c", "_d", "_e", "_f", "_g"];
-    // let problem_url: Vec<String> = vec![];
+    // let problem_id = vec!["_a"];
 
-    for id in problem_id {
-        let problem_url = format!("{url}{id}");
-        let body = reqwest::blocking::get(problem_url)?.text()?;
-        let document = scraper::Html::parse_document(&body);
-        let selector = scraper::Selector::parse("div.part > section > pre").unwrap();
-        let elements = document.select(&selector);  
+    //for id in problem_id {
+    //    let problem_url = format!("{url}{id}");
+    //    let body = reqwest::blocking::get(problem_url)?.text()?;
+    //    let document = scraper::Html::parse_document(&body);
+    //    let selector = scraper::Selector::parse("div.part > section > pre").unwrap();
+    //    let element = document.select(&selector);
+    //
+    //    element.for_each(|e| println!("{}", e.text().next().unwrap()));
+    //}
 
-        elements.for_each(|e| println!("{}", e.text().next().unwrap()));
-    }
+    let problem_url = format!("{url}_a");
+
+    let body = reqwest::blocking::get(problem_url)?.text()?;
+    let document = scraper::Html::parse_document(&body);
+
+    let selector = scraper::Selector::parse("div.part").unwrap();
+    let section = scraper::Selector::parse("section").unwrap();
+    let pre = scraper::Selector::parse("pre").unwrap();
+
+    let element: Vec<_> = document
+        .select(&selector)
+        .flat_map(|item| item.select(&section))
+        .flat_map(|item| item.select(&pre))
+        .map(|item| item.inner_html())
+        .collect();
+
+    println!("{:?}", element);
+
     Ok(())
 }
 
