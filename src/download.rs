0d704e1a86b4a46be5c0ad8201623368ccdff266
kame1366 <110384579+kame1366@users.noreply.github.com>
2024-11-15 16:37:07 +0900

    Refacted download.rs

--- src/download.rs
+++ src/download.rs
@@ -1,13 +1,23 @@
-fn get_samplecase(url_id: String) -> Result<(), Box<dyn std::error::Error>> {
+use std::error::Error;
+
+fn get_samplecase(url: String) -> Result<Vec<String>, Box<dyn Error>> {
+    let body = reqwest::blocking::get(url)?.text()?;
+    let document = scraper::Html::parse_document(&body);
+    let selector = scraper::Selector::parse("span.lang-ja > div.part > section > pre").unwrap();
+    let element: Vec<_> = document
+        .select(&selector)
+        .map(|item| item.inner_html())
+        .collect();
+
+    Ok(element)
+}
+
+fn get_from_id(url_id: String) -> Result<(), Box<dyn Error>> {
     let url = format!("https://atcoder.jp/contests/{url_id}/tasks/{url_id}");
     let problem_id = vec!["a", "b", "c", "d", "e", "f", "g"];
 
     for id in problem_id {
-        let problem_url = format!("{url}_{id}");
-        let body = reqwest::blocking::get(problem_url)?.text()?;
-        let document = scraper::Html::parse_document(&body);
-        let selector = scraper::Selector::parse("span.lang-ja > div.part > section > pre").unwrap();
-        let element: Vec<_> = document.select(&selector).map(|item| item.inner_html()).collect();
+        let element = get_samplecase(format!("{url}_{id}"))?;
         element.iter().for_each(|e| println!("{}", e));
     }
 
@@ -18,5 +28,5 @@ pub fn download(url_id: String) {
     println!("Hello from download!");
     println!("I will download from {}!", url_id);
 
-    let _ = get_samplecase(url_id);
+    let _ = get_from_id(url_id);
 }
