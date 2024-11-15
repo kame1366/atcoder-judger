2814613876b9dc5dd125143c6429d54a8b4677cb
kame1366 <110384579+kame1366@users.noreply.github.com>
2024-11-15 16:29:11 +0900

    Improved getting samplecase

--- src/download.rs
+++ src/download.rs
@@ -1,34 +1,15 @@
 fn get_samplecase(url_id: String) -> Result<(), Box<dyn std::error::Error>> {
     let url = format!("https://atcoder.jp/contests/{url_id}/tasks/{url_id}");
-    // let problem_id = vec!["_a"];
+    let problem_id = vec!["a", "b", "c", "d", "e", "f", "g"];
 
-    //for id in problem_id {
-    //    let problem_url = format!("{url}{id}");
-    //    let body = reqwest::blocking::get(problem_url)?.text()?;
-    //    let document = scraper::Html::parse_document(&body);
-    //    let selector = scraper::Selector::parse("div.part > section > pre").unwrap();
-    //    let element = document.select(&selector);
-    //
-    //    element.for_each(|e| println!("{}", e.text().next().unwrap()));
-    //}
-
-    let problem_url = format!("{url}_a");
-
-    let body = reqwest::blocking::get(problem_url)?.text()?;
-    let document = scraper::Html::parse_document(&body);
-
-    let selector = scraper::Selector::parse("div.part").unwrap();
-    let section = scraper::Selector::parse("section").unwrap();
-    let pre = scraper::Selector::parse("pre").unwrap();
-
-    let element: Vec<_> = document
-        .select(&selector)
-        .flat_map(|item| item.select(&section))
-        .flat_map(|item| item.select(&pre))
-        .map(|item| item.inner_html())
-        .collect();
-
-    println!("{:?}", element);
+    for id in problem_id {
+        let problem_url = format!("{url}_{id}");
+        let body = reqwest::blocking::get(problem_url)?.text()?;
+        let document = scraper::Html::parse_document(&body);
+        let selector = scraper::Selector::parse("span.lang-ja > div.part > section > pre").unwrap();
+        let element: Vec<_> = document.select(&selector).map(|item| item.inner_html()).collect();
+        element.iter().for_each(|e| println!("{}", e));
+    }
 
     Ok(())
 }
