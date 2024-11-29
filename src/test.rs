393fa2a0cc68f947e8a3c60c2beeb9baab1527ff
mataneko <110384579+kame1366@users.noreply.github.com>
2024-11-29 16:59:46 +0900

    Created an execution mechanism in C++

--- src/test.rs
+++ src/test.rs
@@ -1,3 +1,32 @@
-pub fn test(problem: String) {
-    println!("I will test {}...", problem);
+use std::{path, process::{Command, Stdio}};
+
+fn run_script(problem_id: String) {
+    let input_path = path::PathBuf::from(format!(".atcoder-judger\\{problem_id}\\in"));
+    let input_files = input_path.read_dir().unwrap();
+
+    for files_entry in input_files {
+        let file = files_entry.unwrap().path();
+
+        let _compile = Command::new("g++")
+            .args(["-o", "main", "main.cpp"])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .output()
+            .unwrap();
+
+        let output = Command::new("./main")
+            .args(["<", &file.into_os_string().into_string().unwrap()])
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .output()
+            .unwrap();
+
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    }
+}
+
+pub fn test(problem_id: String) {
+    println!("I will test {}...", problem_id);
+
+    run_script(problem_id);
 }
